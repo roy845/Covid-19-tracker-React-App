5a06054526ebe4ca63f96f9df0c4505780f4edc4
roy845 <royatali94@gmail.com>
2021-10-14 14:22:50 +0300

    ds

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const pg = require ('pg');
 const path = require('path');
-
+const bcrypt = require('bcrypt');
 
 
 
